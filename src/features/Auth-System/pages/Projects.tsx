fc99c0b05a7b25b0cb04b7c6ce2a9e393e68a998
Sandeep.S <saligantisandeepzzz6@gmail.com>
2026-09-19 01:17:08 +0530

    feat: add pagination API and query setup

--- src/features/Auth-System/pages/Projects.tsx
+++ src/features/Auth-System/pages/Projects.tsx
@@ -1,4 +1,36 @@
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
+import privateApiClient from "../../../api/privateApiClient";
+
 const Projects = () => {
+  const queryClient = useQueryClient();
+
+  const [page, setPgae] = useState(2);
+  const [limit, setLimit] = useState(10);
+
+  const cacheQuery = useQuery({
+    queryKey: ["projects", page, limit],
+    queryFn: GetProjects,
+  });
+
+  if (cacheQuery.isFetching && cacheQuery.isPending) {
+    console.log("Loading Profile...");
+  } else if (cacheQuery.isFetching) {
+    console.log("Background Refetch is happening");
+  } else {
+    console.log(cacheQuery.data);
+  }
+
+  async function GetProjects() {
+    const projects = await privateApiClient(
+      `/projects?page=${page}&limit=${limit}`,
+      "GET",
+      null,
+    );
+    console.log(projects);
+    return projects;
+  }
+
   return <main>Projects</main>;
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools/production";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
@@ -17,6 +18,7 @@ createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={query}>
     <StrictMode>
       <App />
+      <ReactQueryDevtools />
     </StrictMode>
   </QueryClientProvider>,
 );
